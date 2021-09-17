530b4d5c5d702c082292a7dd03592f0a4bbe394d
medtik <longpham6595@gmail.com>
2021-09-18 02:44:24 +0700

    Inheritance and class syntax C++

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,29 +2,36 @@
 #include <iostream>
 using namespace std;
 
-
-struct ConNguoi
+class ConNguoi
 {
+public:
     string ho_ten;
     int tuoi;
-    float chieu_cao;
-    float can_nang;
+    double chieu_cao;
+    double can_nang;
     int sd3v[3];
-    float bmi = can_nang / pow(chieu_cao,2);
+    
 };
 
-struct humanBMI(ConNguoi)
+class Humanbmi : public ConNguoi
 {
+public:
+    double bmi;
+};
 
-}
+double calc_bmi(double height, double weight)
+{
+    return (weight / pow(height, 2));
+};
 
 int main()
 {
-    ConNguoi nguoi;
+    Humanbmi nguoi;
     nguoi.ho_ten = "Vu Nguyen Coder";
     nguoi.tuoi = 18;
     nguoi.chieu_cao = 1.65;
     nguoi.can_nang = 60;
-    cout << "BMI cua " << nguoi.ho_ten << " la: " << nguoi.bmi << endl;
+    nguoi.bmi = calc_bmi(nguoi.chieu_cao, nguoi.can_nang);
+    cout << "BMI cua " << nguoi.bmi << " la: " <<  nguoi.bmi << endl;
     //BMI(nguoi);
 }
